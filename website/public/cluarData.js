ee61ca8df87befad12755a9073bc982195d54cc1
eduardo.velasques <eduardo.velasques@sitana.pt>
2022-02-08 20:51:29 +0000

    Configuração e imagens do website adicionadas.

---
+++ website/public/cluarData.js
@@ -0,0 +1,324 @@
+window.cluarData = {
+    "dictionary": {"PT": {
+        "footer-code-link": "<p>https://github.com/eduveks/ashtanga<br><\/p>",
+        "footer-code-description": "<p>C\u00F3digo Fonte Aberto<\/p>"
+    }},
+    "pages": {"PT": [
+        {
+            "parent": "",
+            "keywords": "",
+            "sorter": 10,
+            "menu_title": "",
+            "link": "/",
+            "description": "",
+            "menu": true,
+            "title": "Home",
+            "structure": [
+                {
+                    "image": "Banner1-O6lMXZfj.jpg",
+                    "sorter": 0,
+                    "image_alt": "",
+                    "section": "banner",
+                    "image_title": "",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "title": "Foco no Fluxo da Respira\u00E7\u00E3o",
+                    "type": "default",
+                    "actions": [],
+                    "content": "<p>Mantendo a respira\u00E7\u00E3o cont\u00EDnua e profunda durante toda a pr\u00E1tica, nos movimentos ascendentes inspira e nos movimentos descendentes expira.<\/p>"
+                },
+                {
+                    "image": "",
+                    "image_max_width": "0",
+                    "sorter": 20,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Consist\u00EAncia",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p>Ao praticar com consist\u00EAncia, apenas ser\u00E1 necess\u00E1rio tempo e muitas capacidades naturais ser\u00E3o desbloqueadas.&nbsp;<\/p>\r\n<br>\r\n<div class=\"iframe__container\"><iframe frameborder=\"0\" src=\"//www.youtube.com/embed/dhnMcvEQa3Y\"><\/iframe><\/div>\r\n"
+                },
+                {
+                    "image": "",
+                    "image_max_width": "0",
+                    "sorter": 30,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Concentra\u00E7\u00E3o",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p>O movimento sincronizado com a respira\u00E7\u00E3o eleva a concentra\u00E7\u00E3o no presente, o que permite criar estruturas neurais mais complexas e robustas.<\/p>\r\n<br>\r\n<div class=\"iframe__container\"><iframe frameborder=\"0\" src=\"//www.youtube.com/embed/veNXu4L8Zr0\"><\/iframe><\/div>"
+                }
+            ]
+        },
+        {
+            "parent": "/preparacao",
+            "keywords": "",
+            "sorter": 10,
+            "menu_title": "",
+            "link": "/surya-namaskar-a",
+            "description": "",
+            "menu": true,
+            "title": "Surya Namaskar A",
+            "structure": [
+                {
+                    "image": "Banner---Surya-Namaskar-A-BvCJpIe1.jpg",
+                    "sorter": 10,
+                    "image_alt": "",
+                    "section": "banner",
+                    "image_title": "",
+                    "position": {
+                        "x": "",
+                        "y": "-100px"
+                    },
+                    "title": "Surya Namaskar A",
+                    "type": "secondary",
+                    "actions": [],
+                    "content": "<p>A primeira sequ\u00EAncia de sauda\u00E7\u00E3o ao sol.<\/p>"
+                },
+                {
+                    "image": "",
+                    "sorter": 20,
+                    "image_alt": "",
+                    "section": "listing",
+                    "image_title": "",
+                    "title": "Surya Namaskar A",
+                    "type": "asanas",
+                    "items": [
+                        {
+                            "image": "Samasthiti-AGeEHEoJ.png",
+                            "sorter": 10,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "Samasthiti",
+                            "content": ""
+                        },
+                        {
+                            "image": "1-GTCobZz9.png",
+                            "sorter": 20,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "1",
+                            "content": "<p>Inspirar<\/p>"
+                        },
+                        {
+                            "image": "2-xr9DoQDA.png",
+                            "sorter": 30,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "2",
+                            "content": "<p>Expirar<\/p>"
+                        },
+                        {
+                            "image": "3-EV56Zsf5.png",
+                            "sorter": 40,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "3",
+                            "content": "<p>Inspirar<\/p>"
+                        },
+                        {
+                            "image": "4-Bnt5wIfh.png",
+                            "sorter": 50,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "4",
+                            "content": "<p>Expirar<br><\/p>"
+                        },
+                        {
+                            "image": "5-WZ7g2QC4.png",
+                            "sorter": 60,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "5",
+                            "content": "<p>Inspirar<\/p>"
+                        },
+                        {
+                            "image": "6-Da5qwAYy.png",
+                            "sorter": 70,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "6",
+                            "content": "<p>Expirar e realizar 5 respira\u00E7\u00F5es completas.&nbsp;<\/p>"
+                        },
+                        {
+                            "image": "7-qQANNymc.png",
+                            "sorter": 80,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "7",
+                            "content": "<p>Inspirar<\/p>"
+                        },
+                        {
+                            "image": "8-uYzrWrsL.png",
+                            "sorter": 90,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "8",
+                            "content": "<p>Expirar<\/p>"
+                        },
+                        {
+                            "image": "9-EYvfnHBJ.png",
+                            "sorter": 100,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "9",
+                            "content": "<p>Inspirar<\/p>"
+                        },
+                        {
+                            "image": "Samasthiti-hVpOrujd.png",
+                            "sorter": 110,
+                            "image_alt": "",
+                            "link": "",
+                            "section": "listing_item",
+                            "image_title": "",
+                            "title": "Samasthiti",
+                            "content": ""
+                        }
+                    ],
+                    "content": "<p>\u00C9 a sequ\u00EAncia inicial da sauda\u00E7\u00E3o ao sol do Ashtanga Yoga.<\/p><p>Para dar in\u00EDcio \u00E0 pr\u00E1tica toda a sequ\u00EAncia \u00E9 executada por&nbsp;<b>5 vezes<\/b>&nbsp;consecutivamente.&nbsp;<\/p>"
+                },
+                {
+                    "image": "",
+                    "image_max_width": "0",
+                    "sorter": 30,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Demonstra\u00E7\u00E3o",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p><\/p><div class=\"iframe__container\"><iframe frameborder=\"0\" src=\"//www.youtube.com/embed/uiwiHKkc98I\"><\/iframe><\/div><br><p><\/p>"
+                }
+            ]
+        },
+        {
+            "parent": "",
+            "keywords": "",
+            "sorter": 20,
+            "menu_title": "",
+            "link": "/preparacao",
+            "description": "",
+            "menu": true,
+            "title": "Prepara\u00E7\u00E3o",
+            "structure": [
+                {
+                    "image": "Banner---PreparacIaIo-zvruVRXv.jpg",
+                    "sorter": 10,
+                    "image_alt": "",
+                    "section": "banner",
+                    "image_title": "",
+                    "position": {
+                        "x": "",
+                        "y": "-150px"
+                    },
+                    "title": "Prepara\u00E7\u00E3o",
+                    "type": "secondary",
+                    "actions": [],
+                    "content": "<p>A parte inicial da pr\u00E1tica dos asanas (posi\u00E7\u00F5es) do Ashtanga.<\/p>"
+                },
+                {
+                    "image": "",
+                    "image_max_width": "0",
+                    "sorter": 20,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Surya Namaskar A & B",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p>A pr\u00E1tica do Ashtanga inicia com duas sequ\u00EAncias de sauda\u00E7\u00E3o ao sol (surya namaskar)<\/p>\r\n<p>Come\u00E7a pela sequ\u00EAncia A que \u00E9 executada 5 vezes, seguida pela sequ\u00EAncia B que tamb\u00E9m \u00E9 executada 5 vezes.<\/p>\r\n<p>Segue uma demonstra\u00E7\u00E3o de cada uma das sequ\u00EAncias executada uma \u00FAnica vez.<\/p>\r\n<br>\r\n<div class=\"iframe__container\"><iframe frameborder=\"0\" src=\"//www.youtube.com/embed/9g6VO5iSfCE\"><\/iframe><\/div>"
+                }
+            ]
+        },
+        {
+            "parent": "/preparacao",
+            "keywords": "",
+            "sorter": 20,
+            "menu_title": "",
+            "link": "/surya-namaskar-b",
+            "description": "",
+            "menu": true,
+            "title": "Surya Namaskar B",
+            "structure": [
+                {
+                    "image": "Banner---Surya-Namaskar-B-6KxOSuEg.jpg",
+                    "sorter": 10,
+                    "image_alt": "",
+                    "section": "banner",
+                    "image_title": "",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "title": "Surya Namaskar B",
+                    "type": "secondary",
+                    "actions": [],
+                    "content": "<p>A segunda sequ\u00EAncia de sauda\u00E7\u00E3o ao sol.<\/p>"
+                },
+                {
+                    "image": "",
+                    "image_max_width": "0",
+                    "sorter": 30,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Demonstra\u00E7\u00E3o",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p><\/p><div class=\"iframe__container\"><iframe frameborder=\"0\" src=\"//www.youtube.com/embed/dW0VtH3i7Rw\"><\/iframe><\/div><br><p><\/p>"
+                }
+            ]
+        },
+        {
+            "parent": "/preparacao",
+            "keywords": "",
+            "sorter": 30,
+            "menu_title": "",
+            "link": "/fundamental",
+            "description": "",
+            "menu": true,
+            "title": "Fundamental",
+            "structure": []
+        }
+    ]},
+    "languages": [
+        {
+            "default": true,
+            "code": "PT",
+            "description": "Portugu\u00EAs",
+            "locale": "pt"
+        },
+        {
+            "default": false,
+            "code": "EN",
+            "description": "English",
+            "locale": "en"
+        }
+    ],
+    "configuration": {"GENERIC": {"footer-code-link": "https://github.com/eduveks/ashtanga"}}
+};
